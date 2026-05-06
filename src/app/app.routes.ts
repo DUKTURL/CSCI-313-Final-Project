0c903f0dd808f7a0513d2cf3f65a2cabf4325a4d
DUKTURL <saltyreaper979@gmail.com>
2026-05-06 11:41:07 -0500

    fixed auth routing for real

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import { Calendar } from './calendar/calendar';
 import { UserAuth } from './auth/auth';
 
-export const routes: Routes = [ 
-    {
-        path: '',
-        component: Calendar,
-        title: 'Calendar'
-    },
-    {
-        path: '/auth',
-        component: UserAuth,
-        title: 'Authentication'
-    }
+export const routes: Routes = [
+  {
+    path: '',
+    component: Calendar,
+    title: 'Calendar',
+  },
+  {
+    path: 'auth',
+    component: UserAuth,
+    title: 'Authentication',
+  },
 ];
