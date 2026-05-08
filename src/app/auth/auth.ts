b365e8ef1dd3b826387924564f0ebe1de6ba7e11
DUKTURL <saltyreaper979@gmail.com>
2026-05-08 16:13:31 -0500

    completed user authentication, except for some CSS

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User, UserService } from '../user.service';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'user-auth-root',
@@ -9,8 +10,6 @@ import { User, UserService } from '../user.service';
   styleUrl: 'auth.css',
 })
 export class UserAuth {
-  title = 'practice-firestore-crud';
-
   // Injecting UserService from user.service.ts
   userService = inject(UserService);
 
@@ -18,8 +17,8 @@ export class UserAuth {
   email = signal<string>('');
   password = signal<string>('');
 
-  selectedUser = signal<User | null>(null);
   pageState = signal<string>('');
+  accCreationError = signal<string>('');
 
   // This is the hook method that will be called when the component is initialized
   ngOnInit() {
@@ -28,19 +27,32 @@ export class UserAuth {
   //method called when user clicks on the Add User button.
   addUser() {
     const user: User = { name: this.name(), email: this.email(), password: this.password() };
-    this.userService.addUser(user);
-    this.resetForm();
+    if (user.name.length > 0 && user.email.length > 0 && user.password.length > 0) {
+      this.userService.addUser(user);
+      this.resetForm();
+      this.pageState.set('login');
+      this.accCreationError.set('');
+    } else {
+      this.accCreationError.set('');
+      if (user.name.length == 0) {
+        this.accCreationError.update((value) => value + 'n');
+      }
+      if (user.email.length == 0) {
+        this.accCreationError.update((value) => value + 'e');
+      }
+      if (user.password.length == 0) {
+        this.accCreationError.update((value) => value + 'p');
+      }
+    }
   }
   //resets form
   resetForm() {
     this.name.set('');
     this.email.set('');
     this.password.set('');
-    this.selectedUser.set(null);
   }
 
-  selectUser(user: User) {
-    this.selectedUser.set(user);
+  prepUpdateUser(user: User) {
     this.name.set(user.name);
     this.email.set(user.email);
     this.password.set(user.password);
@@ -48,14 +60,38 @@ export class UserAuth {
 
   updateUser() {
     const user: User = { name: this.name(), email: this.email(), password: this.password() };
-    this.userService.updateUser(this.selectedUser()?.id!, user);
+    this.userService.updateUser(this.userService.getCurrentUser()?.id!, user);
     this.resetForm();
+    this.pageState.set('register');
   }
 
-  loginUser() {}
+  validateUser(): true | false {
+    for (const user of this.userService.users()) {
+      if (
+        user.name.toLowerCase() == this.name().toString().toLowerCase() &&
+        user.email.toLowerCase() == this.email().toString().toLowerCase() &&
+        user.password.toLowerCase() == this.password().toString().toLowerCase()
+      ) {
+        this.resetForm();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  loginUser() {
+    const user: User = { name: this.name(), email: this.email(), password: this.password() };
+    if (this.validateUser()) {
+      this.userService.currentUser.set(user);
+    }
+  }
 
   registerUser() {
     this.resetForm();
-    this.pageState.set("register");
+    this.pageState.set('register');
+  }
+
+  logout() {
+    this.userService.currentUser.set(null);
   }
 }
